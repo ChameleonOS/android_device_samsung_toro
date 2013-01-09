dadee3d8066b1ec50eb96ee68cf110ed241c63c6
0xD34D <clark@scheffsblend.com>
2013-01-08 17:29:45 -0700

    Converting to ChameleonOS device tree
    Change-Id: I4d415968faea1562696b89dc2a910a4d8c182833

--- cos.mk
+++ cos.mk
@@ -5,18 +5,18 @@ PRODUCT_RELEASE_NAME := GN-CDMA
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common COS stuff.
+$(call inherit-product, vendor/cos/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/toro/full_toro.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cos/config/nfc_enhanced.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := toro
-PRODUCT_NAME := cm_toro
+PRODUCT_NAME := cos_toro
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := Samsung
